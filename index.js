cd2d729b23688e7b3c20591e4ebedc894155001d
Artem Bushuev <bushuev.artem98@gmail.com>
2019-03-05 13:15:33 +0300

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const {BigQuery} = require('@google-cloud/bigquery')
+const { configure, getLogger } = require('log4js');
+
+
+const schema = require('./schema')
+const {PORT, GOOGLE_APPLICATION_CREDENTIALS, projectId, tableId, datasetId, LOGGER_FILE_PATH} = require('./.env')
+
+
+const app = express()
+const bigquery = new BigQuery({
+  keyFilename: GOOGLE_APPLICATION_CREDENTIALS,
+  projectId: projectId,
+})
+const logger = getLogger()
+configure({
+  appenders: { cheese: { type: 'file', filename: LOGGER_FILE_PATH } },
+  categories: { default: { appenders: ['cheese'], level: 'trace' } }
+});
+
+app.use(bodyParser.json())
+
+app.all('/', function (req, res) {
+  console.dir(req.body)
+  const { event } = req.body
+
+  event.aliases = event.aliases && event.aliases.toString()
+
+  bigquery
+  .dataset(datasetId)
+  .table(tableId)
+  .insert([event])
+  .then(() => res.send('Test'))
+  .catch(err => {
+    logger.error(err, req.body)
+    res.send(err)
+  })
+})
+
+app.listen(PORT)
+
+// CREATE TABLE
+// bigquery.dataset(datasetId).createTable(tableId, {schema}).then(console.log).catch(console.log)
